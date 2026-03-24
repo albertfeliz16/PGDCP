37af9870e97dfcbf7fc873e40aa1aa8dac5dba07
NMC24122000 <lediamorillo24@gmail.com>
2026-03-23 23:33:29 -0400

    limitar la fecha de nacimiento

--- Models/PerfilUsuario.cs
+++ Models/PerfilUsuario.cs
@@ -2,16 +2,18 @@
 using System.ComponentModel.DataAnnotations;
 namespace PGDCP.Models
 {
+    // Validación personalizada para verificar que el usuario sea mayor de edad
     public class MayorDeEdadAttribute : ValidationAttribute
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             if (value is DateTime fecha)
             {
-                
+                // No puede ser hoy ni una fecha futura
                 if (fecha.Date >= DateTime.Today)
                     return new ValidationResult("La fecha de nacimiento no puede ser hoy ni una fecha futura.");
 
+                // Debe ser mayor de 18 años
                 var edad = DateTime.Today.Year - fecha.Year;
                 if (fecha.Date > DateTime.Today.AddYears(-edad)) edad--;
 
@@ -19,7 +21,7 @@ namespace PGDCP.Models
                     return new ValidationResult("Debes ser mayor de 18 años para registrarte.");
             }
 
-            return ValidationResult.Success!;
+            return ValidationResult.Success;
         }
     }
 
@@ -28,26 +30,28 @@ namespace PGDCP.Models
         [Key]
         public int Id { get; set; }
 
-        public string UserId { get; set; }
+        public string? UserId { get; set; }
 
         [Required(ErrorMessage = "El nombre es obligatorio.")]
         [StringLength(50)]
-        public string Nombre { get; set; }
+        public string? Nombre { get; set; }
 
         [Required(ErrorMessage = "El apellido es obligatorio.")]
         [StringLength(50)]
-        public string Apellido { get; set; }
+        public string? Apellido { get; set; }
 
         [Required(ErrorMessage = "La fecha de nacimiento es obligatoria.")]
         [MayorDeEdad] // Validación personalizada
         public DateTime FechaNacimiento { get; set; }
 
         [Required(ErrorMessage = "El sexo es obligatorio.")]
-        public string Sexo { get; set; }
+        public string? Sexo { get; set; }
 
         [Phone(ErrorMessage = "Formato de teléfono no válido.")]
-        public string Telefono { get; set; }
+        public string? Telefono { get; set; }
 
-        public string Rol { get; set; }
+        public string? Rol { get; set; }
     }
 }
+
+
